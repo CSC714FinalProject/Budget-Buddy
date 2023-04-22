2626cb37e32cf6dd4c06da75a67c91785b69b0e7
jsiggia-38 <jsiggia38@gmail.com>
2023-04-22 12:14:32 -0400

    finished add transaction
    finished the add transaction feature

--- routes/home.jsx
+++ routes/home.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import { Link } from 'react-router-dom';
-import { auth } from '../src/firebase';
+import { auth, db } from '../src/firebase';
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import './home.css';
+import AddTransaction from '../components/AddTransaction';
 
 
 
 function Home() {
     const [user, setUser] = useState({});
+    const [showPopup, setShowPopup] = useState(false);
+    const [transactions, setTransactions] = useState([]);
 
     useEffect(() => {
         const stateChange = onAuthStateChanged(auth, (currentUser) => {
@@ -17,10 +21,40 @@ function Home() {
         return () => stateChange();
       }, []);
 
-      const logout = async () => {
+    useEffect(() => {
+      const fetchTransactions = async () => {
+        const transactionsCollection = collection(db, "transactions");
+        const querySnapshot = await getDocs(transactionsCollection);
+        const transactionsData = querySnapshot.docs.map(doc => doc.data());
+        setTransactions(transactionsData);
+
+      }
+      fetchTransactions();
+
+    }, [transactions])
+
+    const logout = async () => {
         await signOut(auth);
     
       };
+    const handleAddTransaction = async (name, amount) => {
+      const user = auth.currentUser;
+      const userId = user ? user.uid : null;
+
+      if (!userId) {
+        console.error("no user found");
+        return;
+      }
+      await addDoc(collection(db, "transactions"), {
+        name,
+        amount,
+        userId
+      });
+    }
+
+
+
+
 
 
       return (
@@ -28,6 +62,15 @@ function Home() {
     
           <h1>Budget Buddy</h1>
           {user && user.email}
+
+          <button onClick = {() => setShowPopup(true)}>Add Transaction</button>
+          {showPopup && <AddTransaction onClose = {() => setShowPopup(false)} onSubmit = {handleAddTransaction} />}
+
+          <ul>
+            {transactions.map((transaction, index) => (
+              <li key = {index}>{transaction.name}: ${transaction.amount}</li>
+            ))}
+          </ul>
         
           <Link to = "/login"><button onClick = {logout}>Sign Out</button></Link>
         </div>
