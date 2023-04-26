6fb050491f3eba4c4897a2245d715aaa2911662c
Joseph <tookerjt@gmail.com>
2023-04-26 15:02:13 -0400

    add css for signup

--- routes/signup.jsx
+++ routes/signup.jsx
@@ -4,6 +4,8 @@ import { auth, db } from '../src/firebase';
 import { collection, addDoc } from "firebase/firestore";
 import { Navigate, useNavigate, Link } from 'react-router-dom';
 import './signup.css'
+import {AiOutlineMail, AiOutlineIdcard, AiOutlineUser} from 'react-icons/ai'
+import {RiLockPasswordLine} from 'react-icons/ri'
 
 
 function Signup() {
@@ -36,29 +38,35 @@ function Signup() {
   
   return (
     <div className="signup-page">
-      <div>
-        <h1>Sign Up</h1>
-        <input className = "user-signup-field" placeholder = "Email..." value = {signupEmail} onChange = {(event) => {setSignupEmail(event.target.value);}}/>
-        <br />
-        <input className = "user-signup-field" placeholder = "Firstname..." value = {firstName} onChange = {(event) => {setFirstName(event.target.value);}} />
-        <br />
-        <input className = "user-signup-field" placeholder = "Lastname..." value = {lastName} onChange = {(event) => {setLastName(event.target.value);}} />
-        <br />
-        <input className = "user-signup-field" placeholder = "Username" value = {username} onChange = {(event) => {setUsername(event.target.value);}} />
-        <br />
-        <input className = "user-signup-field" placeholder = "Password..." value = {signupPassword} onChange = {(event) => {setSignupPassword(event.target.value);}}/>
-        <br />
-        <input className = "user-signup-field" placeholder = "Confirm Password..." value = {confirmPassword} onChange = {(event) => {setConfirmPassword(event.target.value);}}/>
-        <br />
-
-
-        <button onClick = {signup}>Create User</button>
-
-        <p>Already have an account?  <Link to = "/login">Login</Link></p>
-
-      </div>
-
-    
+        <div className="">
+          <img  className = "logo" src= "../images/BudgetBuddy-logos_transparent.png" alt="error"/>
+        </div>
+        <div className="signup-box">
+          <h1 className="signup-text">Sign Up</h1>
+          <h1 className="signup-email-icon"><AiOutlineMail /></h1>
+          <input className = "email-signup-field" placeholder = "Email..." value = {signupEmail} onChange = {(event) => {setSignupEmail(event.target.value);}}/>
+          <br />
+          <h1 className="signup-name-icon"><AiOutlineIdcard/></h1>
+          <input className = "first-name-field" placeholder = "Firstname..." value = {firstName} onChange = {(event) => {setFirstName(event.target.value);}} />
+          <br />
+          <input className = "last-name-field" placeholder = "Lastname..." value = {lastName} onChange = {(event) => {setLastName(event.target.value);}} />
+          <br />
+          <h1 className="signup-user-icon"><AiOutlineUser/></h1>
+          <input className = "username-signup-field" placeholder = "Username" value = {username} onChange = {(event) => {setUsername(event.target.value);}} />
+          <br />
+          <h1 className="signup-password-icon"><RiLockPasswordLine/></h1>
+          <input className = "password-signup-field" type="password" placeholder = "Password..." value = {signupPassword} onChange = {(event) => {setSignupPassword(event.target.value);}}/>
+          <br />
+          <input className = "cpassword-signup-field" type="password" placeholder = "Confirm Password..." value = {confirmPassword} onChange = {(event) => {setConfirmPassword(event.target.value);}}/>
+          <button className="signup-button" onClick = {signup}>Create User</button>
+          <p className="signup-options">or sign up with</p>
+          <div className="signup-logos">
+            <img className="google-signup" src="../images//google-logo.png"/>
+            <img className="apple-signup" src="../images//apple-logo.png"/>
+            <img className="twitter-signup" src="../images//twitter-logo.png"/>
+          </div>
+          <p className="goto-login-text">Already have an account?  <Link to = "/login">Login</Link></p>
+        </div>
     </div>
   )
 
