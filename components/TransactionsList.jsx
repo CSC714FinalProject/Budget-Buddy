cbd37a66fe28f126465526eb169131431ee6c6a1
jsiggia-38 <jsiggia38@gmail.com>
2023-05-13 15:21:43 -0400

    updates to the calendar
    worked on the calendar feature and made some other design improvements

---
+++ components/TransactionsList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './TransactionList.css';
+
+function TransactionsList({ date, transactions, onClose }) {
+    const filteredTransactions = transactions.filter(transaction => {
+        const transactionDate = new Date(transaction.date);
+        const selectedDate = new Date(date);
+        return transactionDate.toDateString() === selectedDate.toDateString();
+      });
+
+    return (
+        
+        <div className = "transactions-container">
+          <h2 className = "list-title">Transactions for {date.toDateString()}</h2>
+          {filteredTransactions.length > 0 ? (
+            <div className="transaction-list">
+              {filteredTransactions.map((filteredTransaction) => (
+                <div key={filteredTransaction.id} className="transaction">
+                  <p>{filteredTransaction.name}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className = "no-transactions">No transactions found for {date.toDateString()}</p>
+          )}
+          <button onClick = {onClose}>Close</button>
+          
+
+        </div>
+
+
+    );
+
+}
+
+export default TransactionsList;
